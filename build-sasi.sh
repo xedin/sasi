cf170caa7273066731387e36ffbcb06208d9096f
Pavel Yaskevich <xedin@apache.org>
2015-11-06 18:41:37 -0800

    switch build script to cassandra/trunk

--- build-sasi.sh
+++ build-sasi.sh
@@ -1,12 +1,12 @@
-rm -rf cassandra-2.0.17
-git clone --branch cassandra-2.0.17 git@github.com:apache/cassandra.git cassandra-2.0.17
-cd cassandra-2.0.17
+rm -rf cassandra-trunk
+git clone git@github.com:apache/cassandra.git cassandra-trunk
+cd cassandra-trunk
 for i in $(ls ../patches | sort)
 do
   git am $(cd $(dirname "$i") && pwd -P)/../patches/$(basename "$i")
 done
-cp -R ../src/* src/
-cp -R ../test/* test/
+#cp -R ../src/* src/
+#cp -R ../test/* test/
 rm -r interface/thrift/gen-java/*
 ant realclean
 ant gen-thrift-java
